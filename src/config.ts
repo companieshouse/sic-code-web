12f52be6604cb47c9b96228b8d76c59a77928329
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-23 12:36:31 +0000

    Update linter to eslint
    Current linter being used was out of date so update to new linter
    eslint.
    Add files required for eslint
    Address issue highlighted by eslint in various files

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,16 @@
 import ApplicationConfiguration from "./ApplicationConfiguration";
 
 const config: ApplicationConfiguration = {
-    applicationNamespace: "sic-code-web",
-    contextIdPrefix: process.env.CONTEXT_ID_PREFIX || "sic_code_web",
-    env: (process.env.NODE_ENV || "development").toLowerCase(),
-    internalApiBaseUrl: process.env.INTERNAL_API_URL as string,
-    internalApiKey: process.env.CHS_INTERNAL_API_KEY as string,
-    sicCodeApiTimeoutMilliseconds: parseInt(process.env.SIC_CODE_API_TIMEOUT_MILLISECONDS || "10000",10),
-    port: parseInt(process.env.SIC_CODE_WEB_PORT || "3000",10)
-}
+  applicationNamespace: "sic-code-web",
+  contextIdPrefix: process.env.CONTEXT_ID_PREFIX || "sic_code_web",
+  env: (process.env.NODE_ENV || "development").toLowerCase(),
+  internalApiBaseUrl: process.env.INTERNAL_API_URL as string,
+  internalApiKey: process.env.CHS_INTERNAL_API_KEY as string,
+  sicCodeApiTimeoutMilliseconds: parseInt(
+    process.env.SIC_CODE_API_TIMEOUT_MILLISECONDS || "10000",
+    10
+  ),
+  port: parseInt(process.env.SIC_CODE_WEB_PORT || "3000", 10),
+};
 
 export default config;
--- test/services/SicCodeService.test.ts
+++ test/services/SicCodeService.test.ts
@@ -1,47 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 import { SicCodeService } from "../../src/services/SicCodeService";
-import { generateTestData } from '../TestData';
-import { StatusCodes } from 'http-status-codes';
+import { generateTestData } from "../TestData";
+import { StatusCodes } from "http-status-codes";
 
-jest.mock('axios');
+jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 mockedAxios.create = jest.fn(() => mockedAxios);
 
-describe('SicCodeService', () => {
+describe("SicCodeService", () => {
+  const sicCodeService = new SicCodeService();
+  describe("when API call is successful", () => {
+    it("should return search results list", async () => {
+      // given
+      const testData = generateTestData();
+      const searchString = "ABC";
+      const matchPhrase = false;
 
-    describe("when API call is successful", () => {
-        it("should return search results list", async () => {
-        // given
-        const testData = generateTestData();
-        const searchString = 'ABC';
-        const matchPhrase = false;
+      mockedAxios.post.mockImplementationOnce(() =>
+        Promise.resolve({
+          status: StatusCodes.OK,
+          data: testData,
+        })
+      );
 
-        mockedAxios.post.mockImplementationOnce(() => Promise.resolve({
-            status: StatusCodes.OK,
-            data: testData
-          }));
-
-        // when
-        const sicCodeService = new SicCodeService();
-        const result = await sicCodeService.search(searchString, matchPhrase);
-        // then
-        expect(result).toEqual(testData);
-        });
+      // when
+      const result = await sicCodeService.search(searchString, matchPhrase);
+      // then
+      expect(result).toEqual(testData);
     });
-    describe("when API call fails", () => {
-        it("should re-throw the error with the correct status code", async () => {
-          // given
-          mockedAxios.post.mockReturnValue(Promise.reject({
-            response: {status: StatusCodes.SERVICE_UNAVAILABLE}
-          }));
-          const searchString = 'ABC';
-          const matchPhrase = false;
-          // when
-          const sicCodeService = new SicCodeService();
-          await sicCodeService.search(searchString, matchPhrase).catch((err) => {
-            // then
-            expect(expect(err.response.status).toBe(503));
-          });
-        });
+  });
+  describe("when API call fails", () => {
+    it("should re-throw the error with the correct status code", async () => {
+      // given
+      mockedAxios.post.mockReturnValue(
+        Promise.reject({
+          response: { status: StatusCodes.SERVICE_UNAVAILABLE },
+        })
+      );
+      const searchString = "ABC";
+      const matchPhrase = false;
+
+      // when
+      await sicCodeService.search(searchString, matchPhrase).catch((err) => {
+        // then
+        expect(expect(err.response.status).toBe(503));
       });
+    });
+  });
 });
