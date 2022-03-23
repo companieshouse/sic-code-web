c2843ac6bf108b25e86e0664f2b88386c0cc36de
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-23 11:01:50 +0000

    update env var name

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ const config: ApplicationConfiguration = {
     internalApiBaseUrl: process.env.INTERNAL_API_URL as string,
     internalApiKey: process.env.CHS_INTERNAL_API_KEY as string,
     sicCodeApiTimeoutMilliseconds: parseInt(process.env.SIC_CODE_API_TIMEOUT_MILLISECONDS || "10000"),
-    port: parseInt(process.env.OCR_WEB_PORT || "3000")
+    port: parseInt(process.env.SIC_CODE_WEB_PORT || "3000")
 }
 
 export default config;
