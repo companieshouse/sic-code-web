2474cbbbe88ba1343d69c248a5db3465ad1430b5
JoshFen95 <jfennell@companieshouse.gov.uk>
2022-03-25 18:10:51 +0000

    Add assertionPattern required for controller test
    Required for SicSearchController.test.ts
    Left out of last commit by mistake

--- test/controllers/HtmlPatternAssertions.ts
+++ test/controllers/HtmlPatternAssertions.ts
@@ -1,12 +1,23 @@
 export function expectToHaveTitle(body: string, expectedTitle: string): void {
   // eslint-disable-next-line
   const patternStr = `<title>\\s*${expectedTitle}\\s*<\/title>`;
-  const pattern = new RegExp(patternStr, 's');
+  const pattern = new RegExp(patternStr, "s");
   expect(body).toMatch(pattern);
 }
 
-export function expectToHaveTableRow(body: string, sicCode: string, sicDescription: string, economicActivity: string): void {
+export function expectToHaveTableRow(
+  body: string,
+  sicCode: string,
+  sicDescription: string,
+  economicActivity: string
+): void {
   const patternStr = `<tr class="govuk-table__row">.*>${sicCode}<.*>${sicDescription}<.*>${economicActivity}<.*</tr>`;
+  const pattern = new RegExp(patternStr, "s");
+  expect(body).toMatch(pattern);
+}
+
+export function expectToHaveErrorSummaryContaining(body: string, errorMessage: string): void {
+  const patternStr = `<span.*class="govuk-error-summary">.*${errorMessage}.*<\/span>`;
   const pattern = new RegExp(patternStr, 's');
   expect(body).toMatch(pattern);
 }
