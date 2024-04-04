8e8dcc40372c16c63bf3d0714222abbc93623664
JoshFen95 <jfennell@companieshouse.gov.uk>
2024-04-04 12:33:19 +0100

    trigger pipeline to test

--- test/controllers/HealthcheckController.test.ts
+++ test/controllers/HealthcheckController.test.ts
@@ -5,7 +5,7 @@ describe('HealthcheckController', () => {
 
   it('should return 200', async () => {
     await request(app)
-      .get("/sic-code-search/healthcheck")
+      .get("/sic-code-search/healthchecks")
       .expect(response => {
         expect(response.status).toEqual(StatusCodes.OK);
       });
